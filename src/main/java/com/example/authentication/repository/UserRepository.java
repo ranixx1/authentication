d9ddeff0a7aa033084ad9bf65024389aeffbbbab
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-04-08 19:53:31 -0300

    feat: simples repository para listagem de USER

---
+++ src/main/java/com/example/authentication/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.example.authentication.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.authentication.enums.Type;
+import com.example.authentication.model.User;
+
+@Repository
+
+public interface UserRepository extends JpaRepository<Integer, User> {
+    
+    Optional<User> findByCpf(String cpf);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByNumberPhone(String numberPhone);
+    List<User> findByType(Type type);
+}
