d8ef4992cfe65d06d0be61b1d26956d9b83ffa21
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-04-08 19:53:56 -0300

    feat: inicia service, mas sem ideias por enquanto

---
+++ src/main/java/com/example/authentication/service/UserService.java
@@ -0,0 +1,17 @@
+package com.example.authentication.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.authentication.repository.UserRepository;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+
+        
+}
