57c2d89bdca280bce6b509efab0e2a756326f6f6
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-04-19 19:53:33 -0300

    feat: adicionando função ao service

--- src/main/java/com/example/authentication/service/UserService.java
+++ src/main/java/com/example/authentication/service/UserService.java
@@ -2,24 +2,35 @@ package com.example.authentication.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.authentication.model.User;
 import com.example.authentication.repository.UserRepository;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
 
-
     // Definir limites de tentativas, padrão de sistema, é 5.
-    // Criar reset de senhas, seja com um Recuperar senha, ou com o acerto ((((ARMAZENAR ESSA LOG!))))
+    // Criar reset de senhas, seja com um Recuperar senha, ou com o acerto
+    // ((((ARMAZENAR ESSA LOG!))))
     // Caso não se recorde : active = false ---Temporariamente.
 
-
-
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    public boolean activateTwoFactorAuthentication(Long id) {
 
-    public 
-        
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuaário não encontrado"));
+
+
+        if (user.isTwoFactorAuthentication() == true) {
+            throw new IllegalArgumentException("Autenticação de dois fatores já está ativa.");
+        }
+
+        user.setTwoFactorAuthentication(true);
+        userRepository.save(user);
+        return true;
+    }
+
 }
