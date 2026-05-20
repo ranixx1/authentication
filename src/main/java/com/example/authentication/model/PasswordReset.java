2c0b732f8259aa32471dd260eefb4e597e490138
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-05-20 20:42:15 -0300

    feat: add PasswordReset entity for managing password reset tokens

---
+++ src/main/java/com/example/authentication/model/PasswordReset.java
@@ -0,0 +1,51 @@
+package com.example.authentication.model;
+
+import java.time.LocalDateTime;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.ManyToOne;
+
+import lombok.Setter;
+
+@Entity
+@Table(name = "password_reset_tokens")
+@Getter
+@Setter
+
+public class PasswordReset {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    private LocalDateTime expireAt;
+    private LocalDateTime createdAt;
+
+    private boolean isUsed = false;
+    
+
+    @PrePersist
+    public void PrePersist(){
+        this.createdAt = LocalDateTime.now();
+    }
+}
+
+/*
+id, user, token, expiração, usado?, data de criação. OK
+*/
