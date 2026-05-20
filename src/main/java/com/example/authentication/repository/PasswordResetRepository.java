c1a47f97661174e5013a8eb8b5d6a13d366215c0
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-05-20 20:42:26 -0300

    feat: create PasswordResetRepository for managing password reset tokens

---
+++ src/main/java/com/example/authentication/repository/PasswordResetRepository.java
@@ -0,0 +1,13 @@
+package com.example.authentication.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.authentication.model.PasswordReset;
+
+@Repository
+public interface PasswordResetRepository extends JpaRepository<PasswordReset, Long> {
+    Optional<PasswordReset> findbyToken(String token);
+}
