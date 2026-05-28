cc82936e9dead7c843f04c9941e445768f62175f
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-05-28 00:01:12 -0300

    feat: repository para busca no histórico

---
+++ src/main/java/com/example/authentication/repository/PasswordHistoryRepository.java
@@ -0,0 +1,16 @@
+package com.example.authentication.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.authentication.enums.PasswordChangeReason;
+import com.example.authentication.model.PasswordHistory;
+import com.example.authentication.model.User;
+
+public interface PasswordHistoryRepository extends JpaRepository <PasswordHistory, Long>{
+    List<PasswordHistory> findByUser(User user);
+    List<PasswordHistory> findByPasswordChangeReason(PasswordChangeReason reason);
+    List<PasswordHistory> findByUserOrderByChangedAtDesc(User user);
+    
+}
