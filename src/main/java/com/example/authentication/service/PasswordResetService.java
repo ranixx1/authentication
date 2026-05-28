713d506bacaeca729f7ad5558e9469df13c90987
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-05-28 00:00:35 -0300

    feat: inicia funcionamento da alteração de pass

--- src/main/java/com/example/authentication/service/PasswordResetService.java
+++ src/main/java/com/example/authentication/service/PasswordResetService.java
@@ -1,32 +1,67 @@
 package com.example.authentication.service;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
 import com.example.authentication.dto.ResetRequest;
+import com.example.authentication.model.PasswordReset;
 import com.example.authentication.model.User;
 import com.example.authentication.repository.PasswordResetRepository;
 import com.example.authentication.repository.UserRepository;
 
 @Service
 public class PasswordResetService {
+
     private final PasswordResetRepository passwordResetRepository;
     private final UserRepository userRepository;
+    private final EmailService emailService;
 
-    public PasswordResetService(PasswordResetRepository passwordResetRepository, UserRepository userRepository) {
+    public PasswordResetService(
+            PasswordResetRepository passwordResetRepository,
+            UserRepository userRepository,
+            EmailService emailService
+        
+        ) {
+
         this.passwordResetRepository = passwordResetRepository;
         this.userRepository = userRepository;
+        this.emailService = emailService;
     }
 
     public String passwordReset(ResetRequest request) {
-        User user = userRepository.findByEmailOrUsername(request.getReset(), request.getReset())
-                .orElseThrow(() -> new RuntimeException("Invalid credentials"));
+
+        User user = userRepository
+                .findByEmailOrUsername(
+                        request.getReset(),
+                        request.getReset()
+                )
+                .orElseThrow(() ->
+                        new RuntimeException("Invalid credentials"));
 
         if (!user.getBirthDate().equals(request.getBirthDate())) {
             throw new RuntimeException("Invalid credentials");
-
         }
-    }
 
+        PasswordReset token = new PasswordReset();
+
+        token.setUser(user);
+
+        token.setToken(UUID.randomUUID().toString());
+
+        token.setExpireAt(
+                LocalDateTime.now().plusMinutes(30)
+        );
+
+        token.setUsed(false);
+
+        passwordResetRepository.save(token);
+
+        emailService.sendResetLink(user.getEmail(), token.getToken());
+
+        return "Recovery email sent";
+    }
 }
 
 /*
