f8bc166ded7bb87c27cd0c00920772ba4aefe564
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-06-01 23:48:01 -0300

    commit vazio

--- src/main/java/com/example/authentication/model/User.java
+++ src/main/java/com/example/authentication/model/User.java
@@ -67,6 +67,7 @@ public class User {
     private Integer failedAttempts = 0; // utilizar service para limitar.
     private LocalDateTime lockUntil; // bloqueia temporariamente
     private boolean twoFactorAuthentication = false;
+    
 
 
     @OneToMany(mappedBy = "user")
