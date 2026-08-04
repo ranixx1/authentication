a150f6896864ea4e9e17975363505bb577c37db3
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-08-04 12:34:03 -0300

    feat: add email sending functionality to reset password feature

--- src/main/java/com/example/authentication/service/EmailService.java
+++ src/main/java/com/example/authentication/service/EmailService.java
@@ -1,21 +1,59 @@
 package com.example.authentication.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+
     private static final Logger log = LoggerFactory.getLogger(EmailService.class);
 
-    public void sendResetLink(
-            String email,
-            String token) {
-        String link = "http://localhost:8080/auth/reset-password?token="
-                + token;
+    private final JavaMailSender mailSender;
 
-        log.info("Sending reset link to: {}", email);
-        log.debug("Reset link: {}", link);
+    @Value("${app.frontend-url}")
+    private String frontendUrl;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
     }
 
+    public void sendResetLink(String email, String token) {
+
+        String link = frontendUrl +
+                "/reset-password?token=" + token;
+
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom(from);
+        message.setTo(email);
+        message.setSubject("Redefinição de senha");
+        message.setText("""
+                Olá,
+
+                Recebemos uma solicitação para redefinir sua senha.
+
+                Clique no link:
+
+                %s
+
+                Caso não tenha solicitado, ignore este email.
+
+                """.formatted(link));
+
+        try {
+            mailSender.send(message);
+            System.out.println("EMAIL ENVIADO");
+        } catch (Exception e) {
+            System.out.println("ERRO EMAIL:");
+            e.printStackTrace();
+            throw e;
+        }
+    }
 }
